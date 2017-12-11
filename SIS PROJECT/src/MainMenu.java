fba159aeabab8cbfd26830ac3df4828ed095f886
Jakob, Rasmussen <jakob.rasmussen@mullenmail.com>
2017-12-11 14:51:37 -0700

    asks first question. inserts options and create method for each of the first choices

--- SIS PROJECT/src/MainMenu.java
+++ SIS PROJECT/src/MainMenu.java
@@ -11,8 +11,45 @@ public class MainMenu
 //				creates new scanner
 				Scanner in = new Scanner(System.in);
 				
+				System.out.println("What would you like to do?");
+				System.out.println("	1) add or delete a student");
+				System.out.println("	2) change student grades/schedule");
+				System.out.println("	3) sort students");
+				int choice1 = in.nextInt();
 				
+				if (choice1 == 1)
+				{
+					addOrDelete();
+				}
+				
+				else if (choice1 == 1)
+				{
+					changeScheduleorGrades();
+				}
+				
+				else if (choice1 == 1)
+				{
+					sortList();
+				}
+				
+		}
 
-			}
+		private static void sortList()
+		{
+			// TODO Auto-generated method stub
+			
+		}
+
+		private static void changeScheduleorGrades()
+		{
+			// TODO Auto-generated method stub
+			
+		}
+
+		private static void addOrDelete()
+		{
+			// TODO Auto-generated method stub
+			
+		}
 
 	}
