3a96cfaa7194a95af730b0c307fd3e16600579ca
Haoxuan Cui <haoxuan.cui@mullenmail.com>
2017-12-11 15:04:51 -0700

    second

---
+++ SIS PROJECT/src/informations.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.text.*;
+public class informations
+	{
+		
+		public static void fillStudentInformation() throws IOException
+			{
+				Scanner file = new Scanner(new File("studentList.txt"));
+			}
+		
+			
+	}
