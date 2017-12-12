b057445a9331dca29ea7c77a8e47bfedfb80b91c
Jakob, Rasmussen <jakob.rasmussen@mullenmail.com>
2017-12-12 14:12:27 -0700

    test

--- SIS PROJECT/src/MainMenu.java
+++ SIS PROJECT/src/MainMenu.java
@@ -7,8 +7,7 @@ public class MainMenu
 		public static void main(String[] args)
 			{
 				System.out.println("Please tell me what you want to do");
-				
-//				creates new scanner
+		
 				Scanner in = new Scanner(System.in);
 				
 				System.out.println("What would you like to do?");
